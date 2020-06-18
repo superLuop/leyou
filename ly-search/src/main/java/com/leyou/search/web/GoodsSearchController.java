5691c6600927e60fdc8d91f2902e6727e39c6f28
superLuop <PLuo2018@163.com>
2020-06-18 17:12:54 +0800

    添加搜索微服务

---
+++ ly-search/src/main/java/com/leyou/search/web/GoodsSearchController.java
@@ -0,0 +1,25 @@
+package com.leyou.search.web;
+
+import com.leyou.common.vo.PageResult;
+import com.leyou.search.model.Goods;
+import com.leyou.search.model.SearchRequest;
+import com.leyou.search.model.SearchResult;
+import com.leyou.search.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GoodsSearchController {
+
+    @Autowired
+    private SearchService searchService;
+
+    @PostMapping("page")
+    public ResponseEntity<SearchResult<Goods>> search(@RequestBody SearchRequest searchRequest) {
+        return ResponseEntity.ok(searchService.search(searchRequest));
+    }
+}
---
+++ ly-search/src/test/java/com/leyou/search/client/CategoryClientTest.java
@@ -0,0 +1,27 @@
+package com.leyou.search.client;
+
+import com.leyou.item.model.Category;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryClientTest {
+
+    @Autowired
+   private CategoryClient categoryClient;
+
+    @Test
+    public void testQueryCategory() {
+        List<Category> list = categoryClient.queryByIds(Arrays.asList(1L, 2L, 3L));
+        list.forEach(System.out::println);
+    }
+
+}
